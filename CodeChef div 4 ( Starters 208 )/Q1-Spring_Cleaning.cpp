1f0001b7ebd0fc275a87ab1804c2fdbf55fbd31e
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-08-09 00:02:11 +0600

    Create Q1-Spring_Cleaning.cpp

---
+++ CodeChef div 4 ( Starters 208 )/Q1-Spring_Cleaning.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int X,Y;
+    cin>>X>>Y;
+    cout<<(X*30)+(Y*60)<<endl;
+    return 0;
+}
