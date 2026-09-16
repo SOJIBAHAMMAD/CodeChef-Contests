8ab607c0abc31ac509271dd9d7e24cba008ff44e
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-17 00:51:07 +0600

    Create Q1-Exun_and_the_Pizzas.cpp

---
+++ CodeChef div 4 ( Starters 213 )/Q1-Exun_and_the_Pizzas.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n,k,r;
+    cin>>n>>k>>r;
+    cout<<(n*r)-(k*r)<<endl;
+    return 0;
+}
