d2a5ad2369f697f18722e278689f00fe9cc74845
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-18 23:58:58 +0600

    Create Q1-Dice_Play.cpp

---
+++ CodeChef div 4 ( Starters 214 )/Q1-Dice_Play.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int a,b;
+   cin>>a>>b;
+   if(a==b) {
+      cout<<"YES"<<endl;
+   } else {
+      cout<<"NO"<<endl;
+   }
+   return 0;
+}
