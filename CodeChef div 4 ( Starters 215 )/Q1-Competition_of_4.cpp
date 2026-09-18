74684242817a47a02d10792e893502a67b86650c
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-19 00:01:09 +0600

    Create Q1-Competition_of_4.cpp

---
+++ CodeChef div 4 ( Starters 215 )/Q1-Competition_of_4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int x;
+   cin>>x;
+   cout<<1000*pow(2,4-x)<<endl;
+   return 0;
+}
