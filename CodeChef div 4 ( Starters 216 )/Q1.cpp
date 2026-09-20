77f67588101f215a506c5b2ff169346f7551ea2f
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-21 00:12:02 +0600

    Create Q1.cpp

---
+++ CodeChef div 4 ( Starters 216 )/Q1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int n;
+   cin>>n;
+   if(n<5) {
+    cout<<n*200<<nl;
+   } else {
+    cout<<1000<<endl;
+   }
+   return 0;
+}
