741a8fe22087fddc0ffc1553d11e2bf29200e986
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-27 00:34:09 +0600

    Add solution for CodeChef problem Q1

---
+++ CodeChef div 4 ( Starters 220 )/Q1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int x;
+   cin>>x;
+   cout<<x*31<<nl;
+   return 0;
+}
